de9e82b5ba5b31576e2298fa574b3da5df5bcbf2
TingtingLiao <tingtingliao000@gmail.com>
2024-06-20 01:24:40 +0400

    init

--- scripts/img2mvimg.py
+++ scripts/img2mvimg.py
@@ -2,66 +2,11 @@ from PIL import Image
 import numpy as np
 import torch
 import os 
-from rembg import remove, new_session
+from rembg import remove
 
 from unique3d_diffusion.model_zoo import build_model 
+from lib.utils import simple_preprocess, change_rgba_bg, rgba_to_rgb, session
   
-providers = [
-    ('CUDAExecutionProvider', {
-        'device_id': 0,
-        'arena_extend_strategy': 'kSameAsRequested',
-        'gpu_mem_limit': 8 * 1024 * 1024 * 1024,
-        'cudnn_conv_algo_search': 'HEURISTIC',
-    })
-] 
-session = new_session(providers=providers) 
-
-def rgba_to_rgb(rgba: Image.Image, bkgd="WHITE"):
-    new_image = Image.new("RGBA", rgba.size, bkgd)
-    new_image.paste(rgba, (0, 0), rgba)
-    new_image = new_image.convert('RGB')
-    return new_image
-
-def change_rgba_bg(rgba: Image.Image, bkgd="WHITE"):
-    rgb_white = rgba_to_rgb(rgba, bkgd)
-    new_rgba = Image.fromarray(np.concatenate([np.array(rgb_white), np.array(rgba)[:, :, 3:4]], axis=-1))
-    return new_rgba
-
-
-def expand2square(pil_img, background_color):
-    width, height = pil_img.size
-    if width == height:
-        return pil_img
-    elif width > height:
-        result = Image.new(pil_img.mode, (width, width), background_color)
-        result.paste(pil_img, (0, (width - height) // 2))
-        return result
-    else:
-        result = Image.new(pil_img.mode, (height, height), background_color)
-        result.paste(pil_img, ((height - width) // 2, 0))
-        return result
-
-
-def simple_preprocess(input_image, rembg_session=session, background_color=255):
-    RES = 2048
-    input_image.thumbnail([RES, RES], Image.Resampling.LANCZOS)
-    if input_image.mode != 'RGBA':
-        image_rem = input_image.convert('RGBA')
-        input_image = remove(image_rem, alpha_matting=False, session=rembg_session)
-
-    arr = np.asarray(input_image)
-    alpha = np.asarray(input_image)[:, :, -1]
-    x_nonzero = np.nonzero((alpha > 60).sum(axis=1))
-    y_nonzero = np.nonzero((alpha > 60).sum(axis=0))
-    x_min = int(x_nonzero[0].min())
-    y_min = int(y_nonzero[0].min())
-    x_max = int(x_nonzero[0].max())
-    y_max = int(y_nonzero[0].max())
-    arr = arr[x_min: x_max, y_min: y_max]
-    input_image = Image.fromarray(arr)
-    input_image = expand2square(input_image, (background_color, background_color, background_color, 0))
-    return input_image
-
 
 def run_mvprediction(trainer, pipeline, image_path: str, remove_bg=True, guidance_scale=1.5, seed=1145):
     input_image = Image.open(image_path) 
@@ -103,12 +48,15 @@ if __name__ == "__main__":
     trainer, pipeline = build_model("img2mvimg", args.ckpt) 
     rgb_pils, single_image = run_mvprediction(trainer, pipeline, args.img, remove_bg=True, guidance_scale=1.5, seed=1145)
 
-    # concate images
-    width, height = rgb_pils[0].size  
-    combined_image = Image.new('RGB', (width * len(rgb_pils), height))
+    img_name = os.path.basename(args.img).split(".")[0]
+    os.makedirs(f"output/{img_name}/images", exist_ok=True)
     for i, img in enumerate(rgb_pils):
-        combined_image.paste(img, (i * width, 0))
-
-    combined_image.save("output.png")
+        img.save(f"output/{img_name}/images/{i}.png")
+    # concate images
+    # width, height = rgb_pils[0].size  
+    # combined_image = Image.new('RGB', (width * len(rgb_pils), height))
+    # for i, img in enumerate(rgb_pils):
+    #     combined_image.paste(img, (i * width, 0))
+    # combined_image.save(f"output/{img_name}/images.png")
    
  
--- scripts/img2normal.py
+++ scripts/img2normal.py
@@ -1,11 +1,50 @@
+import sys
+from PIL import Image
+from typing import List
+
+# sys.path.append("..")
 from unique3d_diffusion.model_zoo import build_model 
+from lib.utils import rgba_to_rgb, simple_remove, rotate_normals_torch, change_rgba_bg
 
-checkpoint_path = "ckpt/image2normal/unet_state_dict.pth"
-trainer, pipeline = build_model("img2normal", checkpoint_path)
+ 
+def predict_normals(trainer, pipeline, image: List, guidance_scale=2., do_rotate=True, num_inference_steps=30, **kwargs):
+    img_list = image if isinstance(image, list) else [image]
+    img_list = [rgba_to_rgb(i) if i.mode == 'RGBA' else i for i in img_list]
+    images = trainer.pipeline_forward(
+        pipeline=pipeline,
+        image=img_list,
+        num_inference_steps=num_inference_steps,
+        guidance_scale=guidance_scale, 
+        **kwargs
+    ).images
+    images = simple_remove(images)
+    if do_rotate and len(images) > 1:
+        images = rotate_normals_torch(images, return_types='pil')
+    return images
 
-# from diffusers import DiffusionPipeline
-# import os
-# model = "lambdalabs/sd-image-variations-diffusers"
+if __name__ == "__main__":
+    import os 
+    import argparse
 
-# model = "~/.cache/huggingface/hub/models--lambdalabs--sd-image-variations-diffusers/snapshots/42bc0ee1726b141d49f519a6ea02ccfbf073db2e/"
-# pipeline = DiffusionPipeline.from_pretrained(os.path.expanduser(model))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ckpt", type=str, default="ckpt/image2normal/unet_state_dict.pth", help="path to the checkpoint")
+    parser.add_argument("--img_dir", type=str, default="output/image/rgb", help="path to the image")
+    args = parser.parse_args()
+
+    trainer, pipeline = build_model("img2normal", args.ckpt) 
+
+    image_files = sorted(os.listdir(args.img_dir))
+    images = [Image.open(os.path.join(args.img_dir, image_file)) for image_file in image_files]
+    normal_pils = predict_normals(trainer, pipeline, images)
+
+    save_dir = os.path.join(os.path.dirname(args.img_dir), "normals")
+    os.makedirs(save_dir, exist_ok=True)
+    for i, normal in enumerate(normal_pils):
+        normal.save(f"{save_dir}/{i}.png")
+    
+    # width, height = normal_pils[0].size  
+    # combined_image = Image.new('RGB', (width * len(normal_pils), height))
+    # for i, img in enumerate(normal_pils):
+    #     img = rgba_to_rgb(img, "white")
+    #     combined_image.paste(img, (i * width, 0))
+    # combined_image.save(f"{os.path.dirname(args.img_dir)}/normals.png")
