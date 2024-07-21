3bd1bada61a27bcf3a4dbf280c459b7812b7f239
TingtingLiao <tingtingliao000@gmail.com>
2024-07-21 17:46:05 +0400

    add demo.py

---
+++ demo.py
@@ -0,0 +1,28 @@
+from PIL import Image
+import numpy as np
+import torch
+import os 
+from rembg import remove
+import torch.nn as nn
+
+from unique3d_diffusion import Unique3dDiffuser
+ 
+ 
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ckpt_dir", type=str, default="ckpt", help="path to the checkpoint")
+    parser.add_argument("--img_dir", type=str, default=None, help="path to image dir")
+    parser.add_argument("--img", type=str, default="data/image.png", help="path to the image")
+    parser.add_argument("--seed", type=int, default=-1, help="random seed") 
+    args = parser.parse_args()
+
+    model = Unique3dDiffuser(args.ckpt_dir, args.seed, save_dir="output")
+    if args.img_dir is not None:  
+        for im_file in sorted(os.listdir(args.img_dir)):
+            model(os.path.join(args.img_dir, im_file))
+    else:
+        model(args.img)
+ 
+    
