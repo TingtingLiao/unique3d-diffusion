433dc78f829e5816eae6bbcef78f5581160cf029
TingtingLiao <tingtingliao000@gmail.com>
2024-07-21 17:56:38 +0400

    update __init__.py

--- unique3d_diffusion/__init__.py
+++ unique3d_diffusion/__init__.py
@@ -122,7 +122,7 @@ class Unique3dDiffuser(nn.Module):
             images = rotate_normals_torch(images, return_types='pil')
         return images
 
-    def forward(self, im_path, remove_bg=False):
+    def forward(self, im_path, remove_bg=False, save=False):
         im_name = os.path.basename(im_path).split(".")[0]  
         front_pil = self.load_image(im_path)
         rgb_pils = self.img2mvimg(front_pil, refine=True)
@@ -152,13 +152,14 @@ class Unique3dDiffuser(nn.Module):
         img_list = [img_list[0]] + erode_alpha(img_list[1:])
         
         # save img_list and mv_normals
-        normal_dir = os.path.join(self.save_dir, im_name, "normals")
-        image_dir = os.path.join(self.save_dir, im_name, "images")
-        os.makedirs(normal_dir, exist_ok=True)
-        os.makedirs(image_dir, exist_ok=True)
-        for i, (img, normal) in enumerate(zip(img_list, mv_normals)):
-            img.save(os.path.join(image_dir, f"{i}.png"))
-            normal.save(os.path.join(normal_dir, f"{i}.png"))
+        if save:
+            normal_dir = os.path.join(self.save_dir, im_name, "normals")
+            image_dir = os.path.join(self.save_dir, im_name, "images")
+            os.makedirs(normal_dir, exist_ok=True)
+            os.makedirs(image_dir, exist_ok=True)
+            for i, (img, normal) in enumerate(zip(img_list, mv_normals)):
+                img.save(os.path.join(image_dir, f"{i}.png"))
+                normal.save(os.path.join(normal_dir, f"{i}.png"))
              
         return img_list, mv_normals
 
